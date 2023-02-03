f8155bd2d30f645dafb6a205dc665c4d57165108
yellowsunn <chohankook95@gmail.com>
2023-02-03 18:05:03 +0900

    edit argocd setting

--- vagrant/k8s_1_26/after_finished/argocd_install.sh
+++ vagrant/k8s_1_26/after_finished/argocd_install.sh
@@ -7,4 +7,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # argocd 초기 비밀번호
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d >> 'argocd_password.txt'
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d >> 'argocd_password.txt'
+mv argocd_password.txt /root

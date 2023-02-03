92b3d6cf7f3ffedbaeb2a6d354175cdfd16bb8f8
yellowsunn <chohankook95@gmail.com>
2023-02-03 17:19:04 +0900

    edit metrics server

--- vagrant/k8s_1_26/after_finished/metric_server_install.sh
+++ vagrant/k8s_1_26/after_finished/metric_server_install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+bin_path="/usr/local/bin"
+
 
 # install metrics-server:3.8.3 by helm
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-helm repo update
+$bin_path/helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+$bin_path/helm repo update
 
-helm install metrics-server metrics-server/metrics-server \
+$bin_path/helm install metrics-server metrics-server/metrics-server \
     --create-namespace \
     --namespace=kube-system \
-    --set hostNetwork.enabled=true \
-    --set args={--kubelet-insecure-tls} \
+    -f metric_server_override.yaml \
     --version 3.8.3

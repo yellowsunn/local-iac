b9316f0eaf3c578996d56fa8ea70ed13d17f09a0
yellowsunn <chohankook95@gmail.com>
2023-03-16 12:17:45 +0900

    kafka 설정 수정

--- vagrant/kafka/kafka_install.sh
+++ vagrant/kafka/kafka_install.sh
@@ -4,6 +4,11 @@
 apt-get update
 apt-get install openjdk-8-jdk -y
 
+# set zookeeper id
+echo $1 > myid
+mkdir /tmp/zookeeper
+cp myid /tmp/zookeeper/myid
+
 # download kafka binary package
 wget https://archive.apache.org/dist/kafka/3.3.1/kafka_2.13-3.3.1.tgz
 tar -zxvf kafka_2.13-3.3.1.tgz
@@ -18,11 +23,6 @@ mv zookeeper.properties config
 wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/kafka-broker-$1.properties
 mv config/server.properties config/server.properties.bak
 mv kafka-broker-$1.properties config/server.properties
-
-# set zookeeper id
-echo $1 > myid
-mkdir /tmp/zookeeper
-cp myid /tmp/zookeeper/myid
 
 # run zookper and kafka server
 bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties

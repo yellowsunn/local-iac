f6c1d31f07fb0494ced681af8dc79a24e69925b1
yellowsunn <chohankook95@gmail.com>
2023-03-16 12:57:24 +0900

    kakfa 설정 수정

--- vagrant/kafka/kafka_install.sh
+++ vagrant/kafka/kafka_install.sh
@@ -12,24 +12,31 @@ cp myid /tmp/zookeeper/myid
 # download kafka binary package
 wget https://archive.apache.org/dist/kafka/3.3.1/kafka_2.13-3.3.1.tgz
 tar -zxvf kafka_2.13-3.3.1.tgz
-cd kafka_2.13-3.3.1
+mv kafka_2.13-3.3.1 /opt/kafka
 
 # override zookeepr.properties
 wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/zookeeper.properties
-mv config/zookeeper.properties config/zookeeper.properties.bak
-mv zookeeper.properties config
+mv /opt/kafka/config/zookeeper.properties /opt/kafka/config/zookeeper.properties.bak
+mv zookeeper.properties /opt/kafka/config
 
 # download server.porperties
 wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/kafka-broker-$1.properties
-mv config/server.properties config/server.properties.bak
-mv kafka-broker-$1.properties config/server.properties
+mv /opt/kafka/config/server.properties /opt/kafka/config/server.properties.bak
+mv kafka-broker-$1.properties /opt/kafka/config/server.properties
+
+# download zookeeper, kafka service
+mkdir -p /usr/lib/systemd/system
+cd /usr/lib/systemd/system
+wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/zookeeper.service
+wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/kafka.service
 
 # run zookper and kafka server
-bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
-bin/kafka-server-start.sh -daemon ./config/server.properties
+# bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
+# bin/kafka-server-start.sh -daemon ./config/server.properties
 
 # reboot setting
-echo "sudo mkdir /tmp/zookeeper" >> /home/vagrant/.profile
-echo "sudo cp /home/vagrant/myid /tmp/zookeeper/myid" >> /home/vagrant/.profile
-echo "sudo /home/vagrant/kafka_2.13-3.3.1/bin/zookeeper-server-start.sh -daemon /home/vagrant/kafka_2.13-3.3.1/config/zookeeper.properties" >> /home/vagrant/.profile
-echo "sudo /home/vagrant/kafka_2.13-3.3.1/bin/kafka-server-start.sh -daemon /home/vagrant/kafka_2.13-3.3.1/config/server.properties" >> /home/vagrant/.profile
+systemctl daemon-reload
+systemctl start zookeeper
+systemctl enable zookeeper
+systemctl start kafka
+systemctl enable kafka

13b33f6961db79912dc689413bde6acfd54d5db7
yellowsunn <chohankook95@gmail.com>
2023-03-16 09:38:52 +0900

    주키퍼 설정 추가

--- vagrant/kafka/kafka_install.sh
+++ vagrant/kafka/kafka_install.sh
@@ -8,3 +8,8 @@ apt-get install openjdk-8-jdk -y
 wget https://archive.apache.org/dist/kafka/3.3.1/kafka_2.13-3.3.1.tgz
 tar -zxvf kafka_2.13-3.3.1.tgz
 cd kafka_2.13-3.3.1
+
+# download server.porperties
+wget https://raw.githubusercontent.com/yellowsunn/local-iac/main/vagrant/kafka/config/kafka-broker-$1.properties
+mv config/server.properties config/server.properties.bak
+mv kafka-broker-1.properties config/server.properties

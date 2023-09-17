ad6c0986f42a0ae288cd6d621a5e544369a9c60b
yellowsunn <chohankook95@gmail.com>
2023-09-18 00:26:30 +0900

    Revert "script 폴더 수정"
    This reverts commit 3a35f1d18af502800ad161cfd8286d06cdc719f4.

--- proxmox/k8s/worker_nodes.sh
+++ proxmox/k8s/worker_nodes.sh
@@ -11,11 +11,8 @@ docker_V='24.0.2-1.el7'     # Docker
 m_node_ip='192.168.0.151'
 ## /configuration variables ##
 
-EXTERNAL_SCRIPTS='https://raw.githubusercontent.com/yellowsunn/local-iac/main/proxmox/k8s/scripts'
-
-
-sh -c "$(curl -fsSL $EXTERNAL_SCRIPTS/k8s_env_build.sh)" $N $m_node_ip
-sh -c "$(curl -fsSL $EXTERNAL_SCRIPTS/k8s_pkg_cfg.sh)" $k8s_V $docker_V $ctrd_V
+sh ./scripts/k8s_env_build.sh $N $m_node_ip
+sh ./scripts/k8s_pkg_cfg.sh $k8s_V $docker_V $ctrd_V
 
 kubeadm join --token 123456.1234567890123456 \
              --discovery-token-unsafe-skip-ca-verification $m_node_ip:6443 \

cee366754148686f4bd10f5eb1d4616af5f0f0a4
yellowsunn <chohankook95@gmail.com>
2023-09-18 00:00:10 +0900

    변수 수정

--- proxmox/k8s/master_node.sh
+++ proxmox/k8s/master_node.sh
@@ -2,17 +2,17 @@
 
 ## configuration variables ##
 # max number of worker nodes
-N = 0
+N=0
 # each of components to install 
-k8s_V    = '1.28.1'           # Kubernetes 
-ctrd_V   = '1.6.21-3.1.el7'   # Containerd 
-docker_V = '24.0.2-1.el7'     # Docker  
+k8s_V='1.28.1'           # Kubernetes 
+ctrd_V='1.6.21-3.1.el7'   # Containerd 
+docker_V='24.0.2-1.el7'     # Docker  
 
 # master node ip
-m_node_ip = '192.168.0.151'
+m_node_ip='192.168.0.151'
 ## /configuration variables ##
 
-EXTERNAL_SCRIPTS = 'https://raw.githubusercontent.com/yellowsunn/local-iac/main/proxmox/k8s/scripts'
+EXTERNAL_SCRIPTS='https://raw.githubusercontent.com/yellowsunn/local-iac/main/proxmox/k8s/scripts'
 
 sh -c "$(curl -fsSL $EXTERNAL_SCRIPTS/k8s_env_build.sh)" $N $m_node_ip
 sh -c "$(curl -fsSL $EXTERNAL_SCRIPTS/scripts/k8s_pkg_cfg.sh)" $k8s_V $docker_V $ctrd_V
--- proxmox/k8s/worker_nodes.sh
+++ proxmox/k8s/worker_nodes.sh
@@ -2,16 +2,16 @@
 
 ## configuration variables ##
 # max number of worker nodes
-N = 3 
+N=3 
 # each of components to install 
-k8s_V    = '1.28.1'           # Kubernetes 
-ctrd_V   = '1.6.21-3.1.el7'   # Containerd 
-docker_V = '24.0.2-1.el7'     # Docker  
+k8s_V='1.28.1'           # Kubernetes 
+ctrd_V='1.6.21-3.1.el7'   # Containerd 
+docker_V='24.0.2-1.el7'     # Docker  
 # master node ip
-m_node_ip = '192.168.0.151'
+m_node_ip='192.168.0.151'
 ## /configuration variables ##
 
-EXTERNAL_SCRIPTS = 'https://raw.githubusercontent.com/yellowsunn/local-iac/main/proxmox/k8s/scripts'
+EXTERNAL_SCRIPTS='https://raw.githubusercontent.com/yellowsunn/local-iac/main/proxmox/k8s/scripts'
 
 
 sh -c "$(curl -fsSL $EXTERNAL_SCRIPTS/k8s_env_build.sh)" $N $m_node_ip
